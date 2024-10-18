f9435a2a03034a3e5ed3bfa2cdea93349bdf1553
ibent95 <ibent95tuny@gmail.com>
2024-10-18 17:18:18 +0700

    Added: New font family

--- src/app/main/opening-remarks/opening-remarks.component.ts
+++ src/app/main/opening-remarks/opening-remarks.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './opening-remarks.component.html',
-  styles: ['section { min-width: 91vw; border: 8px double var(--third-color); border-radius: 1rem; }']
+  styles: ["section { min-width: 91vw; border: 8px double var(--third-color); border-radius: 1rem; }"]
 })
 export class OpeningRemarksComponent {
   @Input() id: string = 'openingRemarks';
